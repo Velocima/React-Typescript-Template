d88c111d1129c70dc38634417219130bddb8cd4f
Velo <hartley.max@outlook.com>
2021-09-21 11:45:15 +0100

    add eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	plugins: ['prettier'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	ignorePatterns: ['.eslintrc.js', 'node_modules', 'build'],
+	extends: [
+		'plugin:prettier/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'plugin:jsx-a11y/recommended',
+		'plugin:eslint-comments/recommended',
+	],
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+		'@typescript-eslint/no-var-requires': 'off',
+		'react/prop-types': 'off',
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'react/display-name': [0, { ignoreTranspilerName: true }],
+		'prettier/prettier': [
+			2,
+			{
+				trailingComma: 'es5',
+				quoteProps: 'as-needed',
+				tabWidth: 2,
+				semi: true,
+				singleQuote: true,
+				printWidth: 100,
+				useTabs: true,
+				bracketSpacing: true,
+				arrowParens: 'always',
+			},
+			{ usePrettierrc: true },
+		],
+	},
+};
